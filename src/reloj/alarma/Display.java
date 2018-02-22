c23e2e92fee9ea2c243d63fc2248f797496d7d7e
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2018-02-22 11:30:39 +0100

    Clases creadas

---
+++ src/reloj/alarma/Display.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reloj.alarma;
+
+import java.awt.Image;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author ablancoabalde
+ */
+public class Display {
+    
+    
+
+}
